787ace161712b932e6fc9b2bad46ee4b20495c6a
Veto2922 <veto2922@gmail.com>
2024-03-05 16:17:47 +0200

    make data set before fun

---
+++ src/data/make_dataset.py
@@ -0,0 +1,120 @@
+import pandas as pd
+from glob import glob
+
+# --------------------------------------------------------------
+# Read single CSV file
+# --------------------------------------------------------------
+single_file_acc = pd.read_csv('../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv')
+
+single_file_gyr = pd.read_csv('../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv')
+
+
+# --------------------------------------------------------------
+# List all data in data/raw/MetaMotion
+# --------------------------------------------------------------
+
+files =  glob('../../data/raw/MetaMotion/*.csv')
+len(files)
+# --------------------------------------------------------------
+# Extract features from filename
+# --------------------------------------------------------------
+f = files[0]
+
+participant = f.split('-')[0][-1]
+
+label= f.split('-')[1]
+
+category = f.split('-')[2].rstrip('1234').rstrip('_MetaWear_2019')
+
+df = pd.read_csv(f)
+
+df['participant'] = participant
+df['label'] = label
+df['category'] = category
+
+# if 'Gyroscope' in  f :
+#     print('Yes')
+
+
+
+# --------------------------------------------------------------
+# Read all files
+# --------------------------------------------------------------
+
+acc_df = pd.DataFrame()
+gyr_df = pd.DataFrame()
+
+# This will be the key for every set (file) 
+acc_set = 1
+gyr_set = 1
+
+for f in files:
+    
+    participant = f.split('-')[0][-1]
+
+    label= f.split('-')[1]
+
+    category = f.split('-')[2].rstrip('1234').rstrip('_MetaWear_2019')
+    
+    df = pd.read_csv(f)
+
+    df['participant'] = participant
+    df['label'] = label
+    df['category'] = category
+    
+    if 'Accelerometer' in f:
+        df['set'] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df])  # Pass the DataFrames as elements of a list
+    
+    if 'Gyroscope' in  f :
+        df['set'] = gyr_set
+        gyr_set += 1
+        gyr_df = pd.concat([gyr_df, df])  # Pass the DataFrames as elements of a list
+
+    
+
+
+
+
+# --------------------------------------------------------------
+# Working with datetimes
+# --------------------------------------------------------------
+
+acc_df.info()
+
+pd.to_datetime(acc_df['epoch (ms)'] , unit='ms') # epoch is on Unix time format
+
+pd.to_datetime(acc_df['time (01:00)'])
+
+#set index as date time
+acc_df.index = pd.to_datetime(acc_df['epoch (ms)'] , unit='ms')
+gyr_df.index = pd.to_datetime(gyr_df['epoch (ms)'] , unit='ms')
+
+# Drop any columns related to date or time 
+acc_df.drop(['epoch (ms)' , 'time (01:00)' , 'elapsed (s)'] , axis=1 , inplace=True)
+gyr_df.drop(['epoch (ms)' , 'time (01:00)' , 'elapsed (s)'] , axis=1 , inplace=True)
+
+
+
+# --------------------------------------------------------------
+# Turn into function
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Merging datasets
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Resample data (frequency conversion)
+# --------------------------------------------------------------
+
+# Accelerometer:    12.500HZ
+# Gyroscope:        25.000Hz
+
+
+# --------------------------------------------------------------
+# Export dataset
+# --------------------------------------------------------------
