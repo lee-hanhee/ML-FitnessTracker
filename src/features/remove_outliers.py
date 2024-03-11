ec0e7bb6ecebaa8733573675d0aaf4d3ea7639dc
Veto2922 <veto2922@gmail.com>
2024-03-11 17:10:55 +0200

    Plot the outliers

--- src/features/remove_outliers.py
+++ src/features/remove_outliers.py
@@ -13,6 +13,8 @@ from sklearn.neighbors import LocalOutlierFactor  # pip install scikit-learn
 df = pd.read_pickle('../../data/interim/01_data_processed.pkl')
 
 
+sensor_col = list(df.columns[:6])
+
 # --------------------------------------------------------------
 # Plotting outliers
 # --------------------------------------------------------------
@@ -22,8 +24,57 @@ mpl.rcParams['figure.figsize'] = (20, 5)
 mpl.rcParams['figure.dpi'] = 100
 
 df[['acc_x' , 'label']].boxplot(by ='label' )
+
+df[sensor_col[:3]  + ['label']].boxplot(by ='label' , figsize = (20, 10)  , layout = (1,3))
+df[sensor_col[3:]  + ['label']].boxplot(by ='label' , figsize = (20, 10)  , layout = (1,3))
+
 plt.show()
 
+def plot_binary_outliers(dataset, col, outlier_col, reset_index):
+    """ Plot outliers in case of a binary outlier score. Here, the col specifies the real data
+    column and outlier_col the columns with a binary value (outlier or not).
+
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): Column that you want to plot
+        outlier_col (string): Outlier column marked with true/false
+        reset_index (bool): whether to reset the index for plotting
+    """
+
+    # Taken from: https://github.com/mhoogen/ML4QS/blob/master/Python3Code/util/VisualizeDataset.py
+
+    dataset = dataset.dropna(axis=0, subset=[col, outlier_col])
+    dataset[outlier_col] = dataset[outlier_col].astype("bool")
+
+    if reset_index:
+        dataset = dataset.reset_index()
+
+    fig, ax = plt.subplots()
+
+    plt.xlabel("samples")
+    plt.ylabel("value")
+
+    # Plot non outliers in default color
+    ax.plot(
+        dataset.index[~dataset[outlier_col]],
+        dataset[col][~dataset[outlier_col]],
+        "+",
+    )
+    # Plot data points that are outliers in red
+    ax.plot(
+        dataset.index[dataset[outlier_col]],
+        dataset[col][dataset[outlier_col]],
+        "r+",
+    )
+
+    plt.legend(
+        ["outlier " + col, "no outlier " + col],
+        loc="upper center",
+        ncol=2,
+        fancybox=True,
+        shadow=True,
+    )
+    plt.show()
 
 # --------------------------------------------------------------
 # Interquartile range (distribution based)
@@ -31,11 +82,46 @@ plt.show()
 
 # Insert IQR function
 
+def mark_outliers_iqr(dataset, col):
+    """Function to mark values as outliers using the IQR method.
+
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): The column you want apply outlier detection to
+
+    Returns:
+        pd.DataFrame: The original dataframe with an extra boolean column 
+        indicating whether the value is an outlier or not.
+    """
+
+    dataset = dataset.copy()
+
+    Q1 = dataset[col].quantile(0.25)
+    Q3 = dataset[col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    dataset[col + "_outlier"] = (dataset[col] < lower_bound) | (
+        dataset[col] > upper_bound
+    )
+
+    return dataset
+
 
 # Plot a single column
 
+col = 'acc_x'
+dataset = mark_outliers_iqr(df , col )
+plot_binary_outliers(dataset=dataset , col=col , outlier_col=col+'_outlier' , reset_index=True)
+
 
 # Loop over all columns
+
+for col in sensor_col:
+    dataset = mark_outliers_iqr(df , col )
+    plot_binary_outliers(dataset=dataset , col=col , outlier_col=col+'_outlier' , reset_index=True)
 
 
 # --------------------------------------------------------------
