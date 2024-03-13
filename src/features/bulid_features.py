bfa70b0b6a9864b68b570efd2152e7ad9fee6c71
Veto2922 <veto2922@gmail.com>
2024-03-13 13:28:02 +0200

    PCA and Sum of squares

--- src/features/bulid_features.py
+++ src/features/bulid_features.py
@@ -1,16 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
-from DataTransformation import LowPassFilter
+from DataTransformation import LowPassFilter , PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
-
 
 # --------------------------------------------------------------
 # Load data
 # --------------------------------------------------------------
 
-df = pd.read_pickle('../../data/interim/02_outliers_removed_chauvenets.pkl')
+df = pd.read_pickle(r'../../data/interim/02_outliers_removed_chauvenets.pkl')
 
 sensor_col = list(df.columns[:6])
 
@@ -73,22 +71,61 @@ LowPass = LowPassFilter()
 
 sampling_frq = 1000 / 200 # # because we are taking the record every 200 ms, so that line calculates number of repetition in 1 sec
 
-cutoff_frq = 1 # the low cutoff frequency, meaning more smoothing in signal
+cutoff_frq = 1.3 # the low cutoff frequency, meaning more smoothing in signal
 
 LowPass.low_pass_filter(df_lowpass , 'acc_y' , sampling_frq , cutoff_frq)
 
+subset = df_lowpass[df_lowpass["set"] == 45]
+print(subset["label"][0])
+
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+ax[0].plot(subset["acc_y"].reset_index(drop=True), label="raw data")
+ax[1].plot(subset["acc_y_lowpass"].reset_index(drop=True), label="butterworth filter")
+ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
+ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
 
 
+for col in sensor_col:
+    df_lowpass = LowPass.low_pass_filter(df_lowpass, col, sampling_frq, cutoff_frq, order=5)
+    df_lowpass[col] = df_lowpass[col + "_lowpass"]
+    del df_lowpass[col + "_lowpass"]
 
 # --------------------------------------------------------------
 # Principal component analysis PCA
 # --------------------------------------------------------------
 
+df_pca = df_lowpass.copy()
+PCA = PrincipalComponentAnalysis()
+pc_values =  PCA.determine_pc_explained_variance(df_pca , sensor_col)
+plt.plot(range(1, 7) , pc_values)
+df_pca = PCA.apply_pca(df_pca , sensor_col , 3 )
+subset = df_pca[df_pca["set"] == 35] 
+
+subset[['pca_1' , 'pca_2' , 'pca_3']].plot()
 
 # --------------------------------------------------------------
 # Sum of squares attributes
 # --------------------------------------------------------------
 
+# To further exploit the data, the scalar magnitudes r of the accelerometer and gyroscope were calculated. 
+# r is the scalar magnitude of the three combined data points: x, y, and z. 
+# The advantage of using r versus any particular data direction is that it is impartial to device orientation and can handle dynamic re-orientations.
+# r is calculated by: r_{magnitude} = sqrt{x^2 + y^2 + z^2}
+
+df_squares = df_pca.copy()
+
+acc_r = df_squares['acc_x'] **2 + df_squares['acc_y'] **2 +df_squares['acc_z'] **2 
+gyr_r = df_squares['gyr_x'] **2 + df_squares['gyr_y'] **2 +df_squares['gyr_z'] **2 
+
+
+df_squares['acc_r'] = np.sqrt(acc_r)
+df_squares['gyr_r'] = np.sqrt(gyr_r)
+
+
+df_squares
+subset = df_squares[df_pca["set"] == 18] 
+
+subset[['acc_r' , 'gyr_r' ]].plot(subplots = True)
 
 # --------------------------------------------------------------
 # Temporal abstraction
